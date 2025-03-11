74ad704a2bee50d436f4f2ffad9bc7e70f123f05
João Suzuki <joaosuzuki98@outlook.com>
2025-03-11 19:29:54 -0300

    feat(route): adiciona componente para roteamento

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HelloWorld() {
+	return (
+		<>
+			<p>
+				Hello World
+			</p>
+		</>
+	)
+}
+
+export default HelloWorld
---
+++ src/pages/Employee/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HelloWorld from '../../components/HelloWorld'
+
+function LoginPage() {
+    return (
+        <div>
+            <HelloWorld />
+        </div>
+    )
+}
+
+export default LoginPage
