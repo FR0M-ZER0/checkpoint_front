e95d0a335e0bc093c15ee8eea75ca3bf6a9b1d9a
João Suzuki <joaosuzuki98@outlook.com>
2025-03-13 19:30:55 -0300

    refactor(style): retira elemento main de dentro do prop

--- src/pages/Employee/MarkingPage.tsx
+++ src/pages/Employee/MarkingPage.tsx
@@ -21,57 +21,55 @@ function MarkingPage() {
                 <div className='flex w-full flex-col text-center justify-center'>
                     <p className='font-light'>01/01/2021</p>
                     <p className='text-xl'>Quarta-feira</p>
-
-                    <Clock/>
-
-                    <main className='mt-12'>
-                        <div onClick={openModal}>
-                            <PointCard
-                                icon={
-                                    <i className="fa-solid fa-door-open text-6xl"></i>
-                                }
-                                period='Início'
-                                time='08:00'
-                                color='main-green-color'
-                            />
-                        </div>
-
-                        <div className='mt-4' onClick={openModal}>
-                            <PointCard
-                                icon={
-                                    <i className="fa-solid fa-mug-hot text-6xl"></i>
-                                }
-                                period='Pausa'
-                                time='12:00'
-                                color='main-blue-color'
-                            />
-                        </div>
-
-                        <div className='mt-4' onClick={openModal}>
-                            <PointCard
-                                icon={
-                                    <i class="fa-solid fa-battery-full text-6xl"></i>
-                                }
-                                period='Retomada'
-                                time='13:00'
-                                color='main-yellow-color'
-                            />
-                        </div>
-
-                        <div className='mt-4' onClick={openModal}>
-                            <PointCard
-                                icon={
-                                    <i class="fa-solid fa-door-closed text-6xl"></i>
-                                }
-                                period='Saída'
-                                time='18:00'
-                                color='main-red-color'
-                            />
-                        </div>
-                    </main>
                 </div>
             }
         >
+            <main className='w-full flex-col text-center'>
+                <Clock/>
+                <div onClick={openModal} className='mt-12'>
+                    <PointCard
+                        icon={
+                            <i className="fa-solid fa-door-open text-6xl"></i>
+                        }
+                        period='Início'
+                        time='08:00'
+                        color='main-green-color'
+                    />
+                </div>
+
+                <div className='mt-4' onClick={openModal}>
+                    <PointCard
+                        icon={
+                            <i className="fa-solid fa-mug-hot text-6xl"></i>
+                        }
+                        period='Pausa'
+                        time='12:00'
+                        color='main-blue-color'
+                    />
+                </div>
+
+                <div className='mt-4' onClick={openModal}>
+                    <PointCard
+                        icon={
+                            <i class="fa-solid fa-battery-full text-6xl"></i>
+                        }
+                        period='Retomada'
+                        time='13:00'
+                        color='main-yellow-color'
+                    />
+                </div>
+
+                <div className='mt-4' onClick={openModal}>
+                    <PointCard
+                        icon={
+                            <i class="fa-solid fa-door-closed text-6xl"></i>
+                        }
+                        period='Saída'
+                        time='18:00'
+                        color='main-red-color'
+                    />
+                </div>
+            </main>
             {
                 isModalVisible &&
                 <Modal title='Marcar ponto' onClose={closeModal}>
