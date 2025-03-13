d99ae03fc0c20946f265eb9cfe1803447673a426
João Suzuki <joaosuzuki98@outlook.com>
2025-03-13 19:27:03 -0300

    feat(style): adiciona cursor pointer para os cards dos pontos

--- src/components/PointCard.tsx
+++ src/components/PointCard.tsx
@@ -9,7 +9,7 @@ type PointCardProp = {
 
 function PointCard({ icon, period, time, color }: PointCardProp) {
     return (
-        <div className={`w-full px-3 py-5 pt-8 flex justify-between ${color} rounded-lg min-h-[116px] items-end`}>
+        <div className={`w-full px-3 py-5 pt-8 flex justify-between cursor-pointer ${color} rounded-lg min-h-[116px] items-end`}>
             <div className='opacity-50'>
                 { icon }
             </div>
