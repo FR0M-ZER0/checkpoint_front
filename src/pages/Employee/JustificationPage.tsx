e854db78f63839330e9ffd1e3778b8ddb153e021
João Suzuki <joaosuzuki98@outlook.com>
2025-03-18 20:54:02 -0300

    feat(fzs-55): adiciona visualização dinamica das faltas

--- src/pages/Employee/JustificationPage.tsx
+++ src/pages/Employee/JustificationPage.tsx
@@ -1,20 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TemplateWithTitle from './TemplateWithTitle'
 import SubmitButton from '../../components/SubmitButton'
 import calendarSVG from '../../assets/calendar.svg'
+import api from '../../services/api'
+import { formatDate } from '../../utils/formatter'
+
+interface Absence {
+    id: number,
+    criadoEm: Date,
+    tipo: 'Atraso' | 'Ausencia'
+}
 
 function JustificationPage() {
     const [fileName, setFileName] = useState<string>('')
+    const [absences, setAbsences] = useState<Absence[]>([])
+    const [selectedType, setSelectedType] = useState<'Atraso' | 'Ausencia'>('Ausencia')
 
     const handleImageClick = (): void => {
         document.getElementById('arquivo')?.click()
     }
+
+    const handleTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedType(event.target.value as 'Atraso' | 'Ausencia')
+    }
+    
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         if (event.target.files && event.target.files.length > 0) {
             setFileName(event.target.files[0].name)
         }
     }
+
+    const fetchAbsences = async () => {
+        try {
+            const response = await api.get('/colaborador/faltas/1')
+            setAbsences(response.data)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        fetchAbsences()
+    }, [])
 
     return (
         <TemplateWithTitle title='Abonar ausência ou atraso'>
@@ -33,12 +61,28 @@ function JustificationPage() {
 
                 <div className='flex w-full justify-between mt-8'>
                     <div className='flex items-center'>
-                        <input type="radio" name='falta' id='ausencia' className='mr-2'/>
+                        <input 
+                            type="radio" 
+                            name='falta' 
+                            id='ausencia' 
+                            className='mr-2' 
+                            value="Ausencia"
+                            checked={selectedType === 'Ausencia'} 
+                            onChange={handleTypeChange} 
+                        />
                         <label htmlFor="ausencia">Ausência</label>
                     </div>
 
                     <div className='flex items-center'>
-                        <input type="radio" name='falta' id='atraso' className='mr-2'/>
+                        <input 
+                            type="radio" 
+                            name='falta' 
+                            id='atraso' 
+                            className='mr-2' 
+                            value="Atraso"
+                            checked={selectedType === 'Atraso'} 
+                            onChange={handleTypeChange} 
+                        />
                         <label htmlFor="atraso">Atraso</label>
                     </div>
                 </div>
@@ -46,7 +90,21 @@ function JustificationPage() {
                 <div className='w-full flex justify-between mt-8'>
                     <div className='w-1/2'>
                         <label htmlFor="data">Data</label>
-                        <input type="date" className='mt-2 mb-4' name='data'/>
+                        <select className='mt-2 mb-4 w-full' name='data'>
+                            {absences.filter(absence => absence.tipo === selectedType).length > 0 ? (
+                                absences
+                                    .filter(absence => absence.tipo === selectedType)
+                                    .map(absence => (
+                                        <option key={absence.id} value={absence.id}>
+                                            {formatDate(absence.criadoEm)}
+                                        </option>
+                                    ))
+                            ) : (
+                                <option disabled selected>
+                                    Nenhuma {selectedType === 'Atraso' ? 'atraso' : 'ausência'} registrada
+                                </option>
+                            )}
+                        </select>
                     </div>
 
                     <div className='w-1/2'>
