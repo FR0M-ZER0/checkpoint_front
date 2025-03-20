720cfff7237379c6b0f9b1815398366eb260571a
João Suzuki <joaosuzuki98@outlook.com>
2025-03-19 23:07:49 -0300

    feat(fzs-55): adiciona interceptor para mudar o cabeçalho de acordo com o tipo de requisição

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -7,4 +7,13 @@ const api = axios.create({
     baseURL: 'http://localhost:8080'
 })
 
+api.interceptors.request.use((config) => {
+    if (config.data instanceof FormData) {
+        config.headers["Content-Type"] = "multipart/form-data"
+    } else {
+        config.headers["Content-Type"] = "application/json"
+    }
+    return config
+})
+
 export default api
