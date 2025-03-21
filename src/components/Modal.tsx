8186504d5369e7d43ed2d7fb626ed2b74d593787
João Suzuki <joaosuzuki98@outlook.com>
2025-03-21 13:05:10 -0300

    feat(fzs-29): arruma largura do modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -23,7 +23,7 @@ function Modal({ title, children, onClose, onSubmit }: ModalProp) {
 
     return (
         <div className='bg-[rgba(0,0,0,0.8)] fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center' onClick={handleBackgroundClick}>
-            <div className='min-w-[80%] bg-white px-4 py-8 rounded'>
+            <div className='max-w-[80%] w-[80%] bg-white px-4 py-8 rounded'>
                 <h2 className='text-2xl mb-4'>
                     { title }
                 </h2>
