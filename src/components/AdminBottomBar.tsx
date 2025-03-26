434a8a6a52dd57ec9db1c6f8d6a24b54be4889c1
João Suzuki <joaosuzuki98@outlook.com>
2025-03-26 15:01:40 -0300

    feat[fzs-35]: adiciona ícones de notificação e marcações no bottom bar de admin

--- src/components/AdminBottomBar.tsx
+++ src/components/AdminBottomBar.tsx
@@ -5,8 +5,18 @@ function AdminBottomBar() {
     return (
         <nav className='main-black-color w-full h-[62px] fixed bottom-0 flex main-white-text items-center justify-evenly'>
             <span>
-                <Link to={'/'}>
+                <Link to={'/admin/dashboard'}>
                     <i className="fa-solid fa-house-chimney text-2xl"></i>
+                </Link>
+            </span>
+            <span>
+                <Link to={'/'}>
+                    <i className="fa-solid fa-timeline text-2xl"></i>
+                </Link>
+            </span>
+            <span>
+                <Link to={'/'}>
+                    <i className="fa-solid fa-bell text-2xl"></i>
                 </Link>
             </span>
         </nav>
