dcbccb5bc5a5400ae786c22d621baadbcddf4a30
João Suzuki <joaosuzuki98@outlook.com>
2025-03-26 13:50:53 -0300

    feat[fzs-35]: adiciona barra de notificações

--- src/pages/Admin/DashboardPage.tsx
+++ src/pages/Admin/DashboardPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import TemplateWithTitle from './TemplateWithTitle'
+import AdminNotificationBar from '../../components/AdminNotificationBar'
 
 function DashboardPage() {
     return (
         <TemplateWithTitle title='Olá gestor 1'>
 
+            <div className='mt-4 w-full'>
+                <AdminNotificationBar text='Notificações' notifications={10}/>
+            </div>
         </TemplateWithTitle>
     )
 }
