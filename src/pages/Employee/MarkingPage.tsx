1bcc801874c989eb25f9f51c6f93e5da55c8c6ed
João Suzuki <joaosuzuki98@outlook.com>
2025-03-27 19:58:43 -0300

    feat[fzs-82]: adiciona tempo atual da marcação ao card

--- src/pages/Employee/MarkingPage.tsx
+++ src/pages/Employee/MarkingPage.tsx
@@ -3,7 +3,7 @@ import TemplateWithFilter from './TemplateWithFilter'
 import Clock from '../../components/Clock'
 import PointCard from '../../components/PointCard'
 import Modal from '../../components/Modal'
-import { formatDate, formatTime } from '../../utils/formatter'
+import { formatDate, formatTime, formatTimeAndMinute } from '../../utils/formatter'
 import api from '../../services/api'
 import { toast } from 'react-toastify'
 
@@ -12,6 +12,10 @@ function MarkingPage() {
     const [dayName, setDayName] = useState<string>('')
     const [currentTime, setCurrentTime] = useState<string>('')
     const [markingType, setMarkingType] = useState<string>('')
+    const [markingStart, setMarkingStart] = useState<string>('')
+    const [markingPause, setMarkingPause] = useState<string>('')
+    const [markingResume, setMarkingResume] = useState<string>('')
+    const [markingEnd, setMarkingEnd] = useState<string>('')
     const d: Date = new Date()
 
     const closeModal = (): void => {
@@ -30,16 +34,29 @@ function MarkingPage() {
         event.preventDefault()
         const data = {
             // TODO: mudar para o id do usuário autenticado
-            colaboradorId: 4,
+            colaboradorId: 8,
             tipo: markingType
         }
 
         try {
             await api.post('/marcacoes', data)
             closeModal()
+            fetchTodayMarkings()
             toast.success('Marcação registrada')
         } catch (err) {
             toast.error("Você já fez essa marcação hoje")
+        }
+    }
+
+    const fetchTodayMarkings = async () => {
+        try {
+            const response = await api.get(`/marcacoes/colaborador/${8}/hoje`)
+            setMarkingStart(formatTimeAndMinute(response.data[0].dataHora))
+            setMarkingPause(formatTimeAndMinute(response.data[1].dataHora))
+            setMarkingResume(formatTimeAndMinute(response.data[2].dataHora))
+            setMarkingEnd(formatTimeAndMinute(response.data[3].dataHora))
+        } catch (err) {
+            console.log(err)
         }
     }
 
@@ -49,6 +66,10 @@ function MarkingPage() {
             'quinta-feira', 'sexta-feira', 'sábado'
         ]        
         setDayName(diasDaSemana[d.getDay()])
+    }, [])
+
+    useEffect(() => {
+        fetchTodayMarkings()
     }, [])
 
     return (
@@ -68,7 +89,7 @@ function MarkingPage() {
                             <i className="fa-solid fa-door-open text-6xl"></i>
                         }
                         period='Início'
-                        time='08:00'
+                        time={markingStart}
                         color='main-green-color'
                     />
                 </div>
@@ -79,7 +100,7 @@ function MarkingPage() {
                             <i className="fa-solid fa-mug-hot text-6xl"></i>
                         }
                         period='Pausa'
-                        time='12:00'
+                        time={markingPause}
                         color='main-blue-color'
                     />
                 </div>
@@ -90,7 +111,7 @@ function MarkingPage() {
                             <i className="fa-solid fa-battery-full text-6xl"></i>
                         }
                         period='Retomada'
-                        time='13:00'
+                        time={markingResume}
                         color='main-yellow-color'
                     />
                 </div>
@@ -101,7 +122,7 @@ function MarkingPage() {
                             <i className="fa-solid fa-door-closed text-6xl"></i>
                         }
                         period='Saída'
-                        time='18:00'
+                        time={markingEnd}
                         color='main-red-color'
                     />
                 </div>
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -8,7 +8,16 @@ const formatTime = (date: Date) => {
     return `${hours}h: ${minutes}min`
 }
 
+const formatTimeAndMinute = (dateString: Date) => {
+    if (!dateString) return ''
+    const date = new Date(dateString)
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0')
+    return `${hours}:${minutes}`
+}
+
 export {
     formatDate,
-    formatTime
+    formatTime,
+    formatTimeAndMinute
 }
