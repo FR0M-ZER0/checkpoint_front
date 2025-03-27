a47f584916543dd6151cdf59e6ba0fe70c3f30fa
João Suzuki <joaosuzuki98@outlook.com>
2025-03-27 15:24:42 -0300

    feat[fzs-82]: adiciona requisição post para marcação de ponto

--- src/pages/Employee/MarkingPage.tsx
+++ src/pages/Employee/MarkingPage.tsx
@@ -11,32 +11,35 @@ function MarkingPage() {
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
     const [dayName, setDayName] = useState<string>('')
     const [currentTime, setCurrentTime] = useState<string>('')
+    const [markingType, setMarkingType] = useState<string>('')
     const d: Date = new Date()
 
     const closeModal = (): void => {
         setIsModalVisible(false)
     }
 
-    const openModal = (): void => {
+    const openModal = (type: string): void => {
         setIsModalVisible(true)
 
         const now = new Date()
         setCurrentTime(formatTime(now))
+        setMarkingType(type)
     }
 
-    const handleSubmit = async (event, type) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
         const data = {
             // TODO: mudar para o id do usuário autenticado
-            colaboradorId: 1,
-            tipo: type
+            colaboradorId: 4,
+            tipo: markingType
         }
 
         try {
-            const response = await api.post('/marcacoes', data)
-            toast.success(response.data)
+            await api.post('/marcacoes', data)
+            closeModal()
+            toast.success('Marcação registrada')
         } catch (err) {
-            toast.error(err)
+            toast.error("Você já fez essa marcação hoje")
         }
     }
 
@@ -59,7 +62,7 @@ function MarkingPage() {
         >
             <main className='w-full flex-col text-center'>
                 <Clock/>
-                <div onClick={openModal} className='mt-12'>
+                <div onClick={() => openModal('ENTRADA')} className='mt-12'>
                     <PointCard
                         icon={
                             <i className="fa-solid fa-door-open text-6xl"></i>
@@ -70,7 +73,7 @@ function MarkingPage() {
                     />
                 </div>
 
-                <div className='mt-4' onClick={openModal}>
+                <div className='mt-4' onClick={() => openModal('PAUSA')}>
                     <PointCard
                         icon={
                             <i className="fa-solid fa-mug-hot text-6xl"></i>
@@ -81,7 +84,7 @@ function MarkingPage() {
                     />
                 </div>
 
-                <div className='mt-4' onClick={openModal}>
+                <div className='mt-4' onClick={() => openModal('RETOMADA')}>
                     <PointCard
                         icon={
                             <i className="fa-solid fa-battery-full text-6xl"></i>
@@ -92,7 +95,7 @@ function MarkingPage() {
                     />
                 </div>
 
-                <div className='mt-4' onClick={openModal}>
+                <div className='mt-4' onClick={() => openModal('SAIDA')}>
                     <PointCard
                         icon={
                             <i className="fa-solid fa-door-closed text-6xl"></i>
@@ -110,15 +113,15 @@ function MarkingPage() {
                         <p>Registrar início: { currentTime }</p>
                     </div>
 
-                    <div className='text-white w-full flex justify-between'>
-                        <button className='main-func-color px-8 py-2 rounded-lg cursor-pointer'>
+                    <form className='text-white w-full flex justify-between' onSubmit={(e) => handleSubmit(e)}>
+                        <button className='main-func-color px-8 py-2 rounded-lg cursor-pointer' type='submit'>
                             Confirmar
                         </button>
 
                         <button className='sec-func-color px-8 py-2 rounded-lg cursor-pointer' onClick={closeModal}>
                             Cancelar
                         </button>
-                    </div>
+                    </form>
                 </Modal>
             }
         </TemplateWithFilter>
