1b38967afcad64324f64551875d48a0d7967b297
João Suzuki <joaosuzuki98@outlook.com>
2025-03-29 17:34:16 -0300

    feat[fzs-35]: adiciona badge de notificações dinâmico

--- src/components/AdminBottomBar.tsx
+++ src/components/AdminBottomBar.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router'
+import { RootState } from '../redux/store'
 
 function AdminBottomBar() {
+    const { count } = useSelector((state: RootState) => state.solicitations)
     return (
         <nav className='main-black-color w-full h-[62px] fixed bottom-0 flex main-white-text items-center justify-evenly'>
             <span>
@@ -14,9 +17,14 @@ function AdminBottomBar() {
                     <i className="fa-solid fa-timeline text-2xl"></i>
                 </Link>
             </span>
-            <span>
+            <span className="relative">
                 <Link to={'/admin/solicitações'}>
                     <i className="fa-solid fa-bell text-2xl"></i>
+                    { count > 0 && (
+                        <span className="absolute top-[-5px] right-[-5px] bg-red-600 text-white text-xs rounded-full px-2">
+                            { count }
+                        </span>
+                    )}
                 </Link>
             </span>
         </nav>
