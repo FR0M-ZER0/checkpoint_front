c48e91c9ddb5cddae4dc5af8784c2d72b5b4809e
Gabriel Guimarães <biel_google@hotmail.com>
2025-03-29 16:22:31 -0300

    feat[fzs-79-77-75]: Implemerntando página de Folgas com agendamento e saldo

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route } from 'react-router'; // Importe Routes e Route de react-router-dom
 
 // Páginas
-import LoginPage from './pages/Employee/LoginPage'
-import MarkingPage from './pages/Employee/MarkingPage'
-import DayPage from './pages/Employee/DayPage'
-import JustificationPage from './pages/Employee/JustificationPage'
-import NotificationsPage from './pages/Employee/NotificationsPage'
-import Ferias from './pages/Ferias/Ferias'
-
+import LoginPage from './pages/Employee/LoginPage';
+import MarkingPage from './pages/Employee/MarkingPage';
+import DayPage from './pages/Employee/DayPage';
+import JustificationPage from './pages/Employee/JustificationPage';
+import NotificationsPage from './pages/Employee/NotificationsPage';
+import Ferias from './pages/Ferias/Ferias';
+import FolgaPage from './pages/Folga/FolgaPage'; // Importe o componente Folga
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/" element={<MarkingPage />} />
-				<Route path="/dia" element={<DayPage />} />
-				<Route path="/ferias" element={<Ferias />} />
-			</Routes>
-			<Routes>
-				<Route path="/abono" element={<JustificationPage />} />
-			</Routes>
-			<Routes>
-				<Route path="/notificacoes" element={<NotificationsPage />} />
-			</Routes>
-		</BrowserRouter>
-	)
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<MarkingPage />} />
+                <Route path="/dia" element={<DayPage />} />
+                <Route path="/ferias" element={<Ferias />} />
+                <Route path="/folgas" element={<FolgaPage />} />
+                <Route path="/abono" element={<JustificationPage />} />
+                <Route path="/notificacoes" element={<NotificationsPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,7 +1,16 @@
 const formatDate = (val: Date | string): string => {
-    return new Date(val).toLocaleDateString('pt-BR')
-}
+    // Se já for uma string no formato dd/MM/yyyy, retorna direto
+    if (typeof val === 'string' && val.match(/^\d{2}\/\d{2}\/\d{4}$/)) {
+        return val;
+    }
+    
+    // Se for Date ou string ISO, converte
+    const date = new Date(val);
+    if (isNaN(date.getTime())) {
+        return 'Data inválida';
+    }
+    
+    return date.toLocaleDateString('pt-BR');
+};
 
-export {
-    formatDate
-}
+export { formatDate };
