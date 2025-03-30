cba56c17b5a1477b6fdc98f722354fabea0cd0da
João Suzuki <joaosuzuki98@outlook.com>
2025-03-30 11:34:05 -0300

    feat[fzs-35]: adiciona lógica para rejeitar ou aceitar solicitação de alteração de ponto

--- src/pages/Admin/SolicitationsPage.tsx
+++ src/pages/Admin/SolicitationsPage.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
 import TemplateWithTitle from './TemplateWithTitle'
 import Filter from '../../components/Filter'
 import SortBy from '../../components/SortBy'
 import AdminNotificationCard from '../../components/AdminNotificationCard'
 import { RootState } from '../../redux/store'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import Modal from '../../components/Modal'
 import { formatDate } from '../../utils/formatter'
+import api from '../../services/api'
+import { toast } from 'react-toastify'
+import { removeSolicitation } from '../../redux/slices/solicitationSlice'
 
 function NotificationsPage() {
     const { solicitations } = useSelector((state: RootState) => state.solicitations)
@@ -14,16 +17,49 @@ function NotificationsPage() {
     const [time, setTime] = useState<string>('')
     const [type, setType] = useState<string>('')
     const [period, setPeriod] = useState<string>('')
+    const [markingId, setMarkingId] = useState<string>('')
+    const [solicitationId, setSolicitationId] = useState<string>('')
+    const dispatch = useDispatch()
 
-    const openModal = (horario: string, tipo: string, periodo: string): void => {
+    const openModal = (horario: string, tipo: string, periodo: string, marcacaoId: string, solicitacaoId: string): void => {
         setIsModalVisible(true)
         setTime(horario)
         setType(tipo)
         setPeriod(periodo)
+        setMarkingId(marcacaoId)
+        setSolicitationId(solicitacaoId)
     }
 
     const closeModal = (): void => {
         setIsModalVisible(false)
+    }
+
+    const handleAccept = async (e: FormEvent): Promise<void> => {
+        e.preventDefault()
+        try {
+            await api.put(`/ajuste-ponto/solicitacao/${solicitationId}`, {status: 'aceito'})
+
+            if (type === 'edicao') {
+                await api.put(`/marcacoes/${markingId}/horario`, {novoHorario: time})
+            } else {
+                await api.delete(`/marcacoes/${markingId}`)
+            }
+
+            toast.success('Ajuste realizado com sucesso')
+            dispatch(removeSolicitation(solicitationId))
+            closeModal()
+        } catch (err: unknown) {
+            console.error(err)
+        }
+    }
+
+    const handleReject = async (): Promise<void> => {
+        try {
+            await api.put(`/ajuste-ponto/solicitacao/${solicitationId}`, {status: 'aceito'})
+            closeModal()
+        } catch (err) {
+            console.error(err)
+        }
     }
     return (
         <TemplateWithTitle title='Solicitações'>
@@ -40,7 +76,7 @@ function NotificationsPage() {
             <div className='mt-8'>
                 {
                     solicitations.map(solicitation => (
-                        <div className='mb-4' onClick={() => openModal(solicitation.horario, solicitation.tipo, solicitation.periodo)}>
+                        <div className='mb-4' onClick={() => openModal(solicitation.horario, solicitation.tipo, solicitation.periodo, solicitation.marcacaoId, solicitation.id)}>
                             <AdminNotificationCard
                                 name='Func. 1'
                                 date={formatDate(solicitation.criadoEm)}
@@ -56,13 +92,39 @@ function NotificationsPage() {
             {
                 isModalVisible &&
                 <Modal title='Solicitação para ajuste no ponto' onClose={closeModal}>
-                    <form action="">
+                    <form onSubmit={handleAccept}>
                         <p>Na marcação de: {period}</p>
-                        <p>Tipo: {type === 'edicao' ? 'edição' : 'exclusão'}</p>
 
-                        <div className='w-full flex flex-col items-center my-6'>
-                            <p className='font-bold text-xl'>Horário</p>
-                            <p className='quicksand text-lg'>{time.slice(0, 5)}</p>
+                        {
+                            type === 'edicao' &&
+                            <p>Edição no horário do ponto</p>
+                        }
+
+                        {
+                            type === 'exclusao' &&
+                            <div className='flex justify-center my-8'>
+                                <p className='font-bold main-red-text text-xl'>
+                                    Exclusão do ponto
+                                </p>
+                            </div>
+                        }
+
+                        {
+                            time &&
+                            <div className='w-full flex flex-col items-center my-8'>
+                                <p className='font-bold text-xl'>Horário</p>
+                                <p className='quicksand text-lg'>{time.slice(0, 5)}</p>
+                            </div>
+                        }
+
+                        <div className='text-white flex justify-between'>
+                            <button className='main-func-color px-8 py-2 rounded-lg mr-4 cursor-pointer'>
+                                Aceitar
+                            </button>
+
+                            <button className='sec-func-color px-8 py-2 rounded-lg cursor-pointer' onClick={handleReject}>
+                                Rejeitar
+                            </button>
                         </div>
                     </form>
                 </Modal>
